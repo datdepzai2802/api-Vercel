5cf94fc5545d7dbd8077b5e70c67aee7ca1f3f2e
ngotiendat <datmau20@gmail.com>
2022-10-19 15:00:46 +0700

    setheader

--- src/index.js
+++ src/index.js
@@ -26,6 +26,10 @@ server.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Max-Age", "1800");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
 
   if (req.method === "POST") {
     req.body.createAt = Date.now();
